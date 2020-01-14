f683dda27f3d5510efec645dcf81581a91ea377a
unknown <selvaggio.adam@gmail.com>
2020-01-14 14:15:01 +0100

    navigation dans tableau

--- fight.py
+++ fight.py
@@ -20,34 +20,56 @@ def recherche(arg): # Je crée la fonction
             
         for anchor in soup.find_all('div', {"class":"mw-parser-output"}):
             for toto in anchor.find_all('a'):
-                titi = str(toto.get('href')).replace("/wiki/","").replace("%27","'").replace("%C3%A8","è").replace("%C3%A9","é").replace('%C3%AA','ê').replace('%C3%A7','ç').replace('%C3%A0','à').replace('%C3%B4','ô')
+                titi = str(toto.get('href')).replace("/wiki/","").replace("%27","'").replace("%C3%A8","è").replace("%C3%A9","é").replace('%C3%AA','ê').replace('%C3%A7','ç').replace('%C3%A0','à').replace('%C3%B4','ô').replace('%C3%89','É')
                 if not ('/w/') in titi:
-                    links.append(titi)
+                    if not ('#') in titi:
+                        if not ('Fichier') in titi:
+                            if not ('http') in titi:
+                                if not ('Modèle') in titi:
+                                    links.append(titi)
                         
         return links
 
-def afficheTableau(tab):
+def afficheTableau(tab,deb,fin):
     i=1
-    for e in tab:
-        print('{} - {}'.format(i,e))
+    for indice in range(deb,fin):
+        print('{} - {}'.format(indice,tab[indice]))
         i+=1
 
 try :
-    success = false
+    success = False
     r = recherche('https://fr.wikipedia.org/wiki/Les_Vacances_de_monsieur_Hulot')
-    # while
-    print('************************ WikiGame **** tour 1')
-    print('Actuellement : {}'.format(r[0]))
-    print('00 - Retour /')
-    afficheTableau(r)
-    nvpage = int(input('Votre choix : '))
-    if nvpage == 0:
-        # do retour
-        i=0
-    else:
-        rn = recherche('https://fr.wikipedia.org/wiki/{}'.format(r[nvpage]))
-
-
+    tour = 0
+    deb=1
+    fin=21
+    while not success:
+        print('************************ WikiGame **** tour {}'.format(tour))
+        print('Actuellement : {}'.format(r[0]))
+        print('00 - Retour /')
+        afficheTableau(r,deb,fin)
+        print('98 - Voir les liens précédents /')
+        print('99 - Voir la suite /')
+        nvpage = int(input('Votre choix : '))
+        if nvpage == 0:
+            # do retour
+            i=0
+        elif nvpage == 99:
+            if deb+20 > len(r):
+                deb = fin+1
+                fin += len(r)-fin
+            else:
+                deb+=20
+                fin+=20
+            afficheTableau(r,deb,fin)
+        elif nvpage == 98:
+            if not deb==1 :
+                deb-=20
+                fin-=20
+            afficheTableau(r,deb,fin)
+        else:
+            rn = recherche('https://fr.wikipedia.org/wiki/{}'.format(r[nvpage]))
+            tour+=1
+        
     # afficheTableau(recherche('https://fr.wikipedia.org/wiki/Les_Vacances_de_monsieur_Hulot'))
     # r1 = recherche('https://fr.wikipedia.org/wiki/Les_Vacances_de_monsieur_Hulot')
     # r2 = recherche('https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard')
